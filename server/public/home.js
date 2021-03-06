4bb2197a790988dc1013e3dc44c535cccb45a2be
euberdeveloper <euberdeveloper@gmail.com>
2021-03-06 18:48:46 +0100

    refactor(server): refactor hbs
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- server/public/home.js
+++ server/public/home.js
@@ -1,12 +1,14 @@
+'use strict';
+
 function copy(type) {
     var input = document.createElement('input');
-    var element= document.getElementById(type);
+    var element = document.getElementById(type);
     input.setAttribute('value', port_number);
     document.body.appendChild(input);
     input.select();
     var risultato = document.execCommand('copy');
     document.body.removeChild(input);
-    alert(type +' copiato: '+ element);
+    alert(type + ' copiato: ' + element);
     return risultato;
 }
 
