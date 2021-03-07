49e95f5c04ff8fbbbb1b90fc0b3a00f25b6d8bec
euberdeveloper <euberdeveloper@gmail.com>
2021-03-07 22:52:01 +0100

    feat(shell): fetch user
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- shell/shi.sh
+++ shell/shi.sh
@@ -10,11 +10,12 @@ fi
 # urls
 api_url="https://eagletrt-telemetria-ip.herokuapp.com/api"
 get_hostname="$api_url/machines/$machine/hostname"
+get_user="$api_url/machines/$machine/user"
 get_port="$api_url/machines/$machine/port"
 
 # fetch data
 hostname="$(curl -s $get_hostname)"
-user="ubuntu"
+user="$(curl -s $get_user)"
 port="$(curl -s $get_port)"
 
 # execute ssh
