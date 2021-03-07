26941c7e1d6644c1f7841fcc554cdf800b41db80
euberdeveloper <euberdeveloper@gmail.com>
2021-03-07 19:40:00 +0100

    fix(client): fix client api
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- client/config.js
+++ client/config.js
@@ -2,5 +2,5 @@ module.exports = {
 	RATE_IN_MILLISECONDS: 5000,
 	GET_PORT_URL: 'http://127.0.0.1:4040/api/tunnels',
 	GET_PUBLIC_IP_URL: 'https://ipinfo.io/ip',
-	POST_PORT_URL: 'http://localhost:3000/eagletrt/telemetria/info'
+	POST_PORT_URL: 'http://localhost:3000/api/machines/telemetria'
 };
