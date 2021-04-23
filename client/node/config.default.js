acca2209d6bd4bef85224ad37abb2aa1085ed067
euberdeveloper <33126163+euberdeveloper@users.noreply.github.com>
2021-04-23 16:05:55 +0200

    fix(client-node): fix zerotier with sudo

--- client/node/config.default.js
+++ client/node/config.default.js
@@ -2,6 +2,6 @@ module.exports = {
 	RATE_IN_MILLISECONDS: 5000,
 	GET_PORT_URL: 'http://127.0.0.1:4040/api/tunnels',
 	GET_PUBLIC_IP_URL: 'https://ipinfo.io/ip',
-	GET_ZEROTIER_INFO_COMMAND: 'zerotier-cli listnetworks',
+	GET_ZEROTIER_INFO_COMMAND: 'sudo zerotier-cli listnetworks',
 	POST_PORT_URL: 'http://eagletrt-telemetria-ip.herokuapp.com/api/machines/telemetria'
 };
