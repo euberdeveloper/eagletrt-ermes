b2413d3139acd0a8d44129a67f575d92a4d7dcb6
euberdeveloper <euberdeveloper@gmail.com>
2021-04-27 01:00:26 +0200

    ci: add docker for test purpose
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ client/config.docker.js
@@ -0,0 +1,7 @@
+module.exports = {
+	RATE_IN_MILLISECONDS: 5000,
+	GET_PORT_URL: 'http://ngrok:4551/api/tunnels',
+	GET_PUBLIC_IP_URL: 'https://ipinfo.io/ip',
+	GET_ZEROTIER_INFO_COMMAND: 'echo -e "\n       xxxxxx 192.168.1.23"',
+	POST_PORT_URL: 'http://server:3000/api/machines/telemetria'
+};
